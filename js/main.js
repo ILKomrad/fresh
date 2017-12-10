e2c2ec0442382f03c4f8df96b4df881f2d6daa1d
ILKomrad <komrad.iljich2015@yandex.ru>
2017-12-10 12:32:07 +0200

    mobile

--- js/main.js
+++ js/main.js
@@ -352,135 +352,233 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+window.addEventListener('load', function() {
+    function s() {
+        this.dist = 45, this.radius = 1.5, this.reactionSensitivity = 4, this.thickness = Math.pow(80, 2), this.oGrilled = {
+            iCol: 0,
+            iLine: 0,
+            aDots: []
+        }, s.prototype.setGrilled = function() {
+            this.oGrilled.aDots = [], this.oGrilled.iCol = Math.ceil(r.w / this.dist), this.oGrilled.iLine = Math.ceil(r.h / this.dist), this.oGrilled.margTop = (r.h - this.oGrilled.iLine * this.dist) / 2, this.oGrilled.margLeft = (r.w - this.oGrilled.iCol * this.dist) / 2
+        }, s.prototype.buildGrilled = function() {
+            for (var e = 0; e <= this.oGrilled.iLine; e++)
+                for (var t = 0; t <= this.oGrilled.iCol; t++) this.addDot(e, t)
+        }, s.prototype.addDot = function(e, t) {
+            this.oGrilled.aDots.push(this.build_dot(e, t))
+        }, s.prototype.build_dot = function(e, t) {
+            var o = Math.ceil(t * this.dist + this.oGrilled.margLeft),
+                s = Math.ceil(e * this.dist + this.oGrilled.margTop);
+            return oDot = {
+                x: o,
+                y: s,
+                bx: o,
+                by: s,
+                tx: o,
+                ty: s,
+                s: rand(.2, 1),
+                c: "rgba( 222, 69, 64, 0.6)"
+            }, oDot
+        }, s.prototype.update = function() {
+            this.update_dots()
+        }, s.prototype.update_dots = function() {
+            for (i = 0; i < this.oGrilled.aDots.length; i++) {
+                var e = this.oGrilled.aDots[i],
+                    t = l.x - e.x,
+                    o = l.y - e.y,
+                    s = (Math.sqrt(t * t + o * o), e),
+                    a = l.x / n.width - .5;
+                theta = Math.atan2(s.y - l.y, s.x - l.x), distance = 100 * this.reactionSensitivity / Math.sqrt((l.x - s.x) * (l.x - s.x) + (l.y - s.y) * (l.y - s.y)) + 10, e.x += Math.cos(theta) * distance * a + .05 * (s.bx - s.x), e.y += Math.sin(theta) * distance * a + .05 * (s.by - s.y), c.beginPath(), c.arc(e.x, e.y, this.radius, 0, 2 * Math.PI, !1), c.fillStyle = e.c, c.fill()
+            }
+        }
+    }
+
+    var r = {
+        h: document.getElementById("portfolio").clientHeight,
+        w: document.getElementById("portfolio").clientWidth
+    },
+    l = {
+        x: r.w / 2,
+        y: r.h / 2
+    },
+    n = document.getElementById("dots-bg"),
+    c = n.getContext("2d");
+    n.height = r.h, n.width = r.w, rand = function(e, t) {
+        return Math.ceil(Math.random() * (t - e) + e)
+    }, update_mouse = function(e) {
+        var t = n.getBoundingClientRect();
+        l.y = Math.round((e.clientY - t.top) / (t.bottom - t.top) * n.height), l.x = Math.round((e.clientX - t.left) / (t.right - t.left) * n.width)
+    }, onresize = function() {
+        r.w = n.width = window.innerWidth, r.h = n.height = document.getElementById("portfolio").clientHeight, d.setGrilled(), d.buildGrilled()
+    };
+    var d = new s;
+    container = document.getElementById("portfolio"), container.addEventListener("mousemove", update_mouse, !1), container.addEventListener("onresize", onresize, !1), window.onresize(), d.setGrilled(), d.buildGrilled(), a()
+
+    function a() {
+        c.clearRect(0, 0, n.width, n.height), d.update(), requestAnimationFrame(a)
+    }
+    // dots end
+})
+
 $(document).ready(function() {
-  $('#arrows > g').each(function() {
-    var th = $(this),
-        offsetLine = th.attr('stroke-dashoffset'),
-        st = th.find('.st1'),
-        offsetSt = st.attr('stroke-dashoffset');
+    var partnerTransition = 0;
 
-        th.css({'stroke-dashoffset': offsetLine})
-        st.css({'stroke-dashoffset': offsetSt})
-  });
+    $('.partner').each(function() {
+        $(this).css('transition-delay', partnerTransition + 's');
+        partnerTransition += 0.05;
+    })
 
-  $('.question__plus').on('click', function() {
-    var question = $(this).closest('.question'),
-        content = question.find('.question__content');
+    $('#arrows > g').each(function() {
+        var th = $(this),
+            offsetLine = th.attr('stroke-dashoffset'),
+            st = th.find('.st1'),
+            offsetSt = st.attr('stroke-dashoffset');
+
+            th.css({'stroke-dashoffset': offsetLine})
+            st.css({'stroke-dashoffset': offsetSt})
+    });
+
+    $('.question__plus').on('click', function() {
+        var question = $(this).closest('.question'),
+            content = question.find('.question__content');
+        
+        $('.question').not(question).removeClass('selected');
+        $('.question__content').not(content).slideUp();
+
+        question.toggleClass('selected');
+        content.slideToggle();
+    })
+
+    $('.js-map-open').on('click', function(e) {
+        e.preventDefault();
+
+        $('.contacts').toggleClass('open');
+    });
     
-    $('.question').not(question).removeClass('selected');
-    $('.question__content').not(content).slideUp();
-
-    question.toggleClass('selected');
-    content.slideToggle();
-  })
-
-  $(' #da-thumbs > li ').hoverdir();
-
-  $('.js-map-open').on('click', function(e) {
-    e.preventDefault();
-
-    $('.contacts').toggleClass('open');
-  })
-
-  $('.employees-table__row').hover(function() {
-      $(this).addClass('selected')
-  }, function() {
-    $(this).removeClass('selected')
-  })
-
-
-
-
-    var ctrl = new ScrollMagic.Controller(),
+    function initMagic() {
+        var ctrl = new ScrollMagic.Controller(),
         portfolioTop = $('.portfolio').offset().top;
 
-    var timeLineStar = new TimelineMax();
-    timeLineStar
-      .to('.decision-svg-hide svg', 1, {scale: 2, delay: 0.5, onComplete: function() {
-        //ctrl.scrollTo('.portfolio')
-      }})
-      .to('.decision-svg-hide .letter', 0.5, {fill: '#ffffff'}, '-=0.6')
-      .to('.decision-svg-hide svg', 1, {opacity: 0})
-      .to('.portfolio', 0, {opacity: 1}, '-=1')
-
-    var scene1 = new ScrollMagic.Scene({
-        triggerElement: '#trig',
-        triggerHook: 'onLeave',
-        duration: '150%',
-    })
-    .on('enter', function() {
-        $('#arrows > g').each(function() {
-        var th = $(this),
-            st = th.find('.st1');
-
-            th.animate({'stroke-dashoffset': 0}, 1000, function() {
-                st.animate({'stroke-dashoffset': 0}, 0)
+        if ($(window).width() <= 991) {
+            var sceneSec2 = new ScrollMagic.Scene({
+                triggerElement: '#sec2',
             })
+            .on('enter', function() {
+                $('#sec2').addClass('done');
+            })
+            .addTo(ctrl);
+        }
+
+        var timeLineStar = new TimelineMax();
+        timeLineStar
+        .to('.decision-svg-hide svg', 1, {scale: 2, delay: 0.5})
+        .to('.decision-svg-hide .letter', 0.5, {fill: '#ffffff'}, '-=1')
+        .to('.decision-svg-hide svg', 0.5, {opacity: 0})
+        .to('.portfolio', 0, {opacity: 1, zIndex: 777}, '-=0.5')
+
+        var scene1 = new ScrollMagic.Scene({
+            triggerElement: '#trig',
+            triggerHook: 'onLeave',
+            duration: '150%',
+        })
+        .on('enter', function() {
+            $('#arrows > g').each(function() {
+                var th = $(this),
+                    st = th.find('.st1');
+
+                    th.animate({'stroke-dashoffset': 0}, 1000, function() {
+                        st.animate({'stroke-dashoffset': 0}, 0)
+                    })
+            });
+        })
+        .setPin('.js-decision__inner')
+        .setTween(timeLineStar)
+        .addTo(ctrl);
+
+        var timeLineAbout = new TimelineMax();
+        timeLineAbout
+            .to('.about-slide:nth-child(1)', 0, 
+                {
+                    className: '+=zoom-in', 
+                    delay: '0.1'
+                }
+            )
+            .to('.about-slide:nth-child(2)', 0, 
+                {
+                    className: '+=active'
+                }
+            )
+            .to('.about-slide:nth-child(2)', 0, 
+                {
+                    className: '+=zoom-in', 
+                    delay: 0.1,
+                    onComplete: function() {
+                        $('.about-slide:nth-child(2)').removeClass('active')
+                    }
+                }
+            )
+            .to('.about-slide:nth-child(3)', 0, 
+                {
+                    className: '+=active'
+                }
+            )
+            .to('.about-slide:nth-child(3)', 0, 
+                {
+                    className: '+=zoom-in', 
+                    delay: 0.1,
+                    onComplete: function() {
+                        $('.about-slide:nth-child(2)').removeClass('active')
+                    }
+                }
+            )
+            .fromTo('.about-slide:nth-child(4)', 0.1, 
+                {
+                className: '-=active', 
+                },
+                {
+                className: '+=active'
+                }
+            )
+    
+        var scene3 = new ScrollMagic.Scene({
+            triggerElement: '.about',
+            triggerHook: 'onLeave',
+            duration: '100%',
+        })
+        .setPin('.about')
+        .setTween(timeLineAbout)
+        .addTo(ctrl);
+
+        var sceneTable = new ScrollMagic.Scene({
+            triggerElement: '.employees-table'
+        })
+        .addTo(ctrl)
+        .on('enter', function() {
+            $('.employees-table').addClass('load').removeClass('start')
+        })
+    }
+
+    if ($(window).width() >= 600) {
+        $(' #da-thumbs > li ').hoverdir();
+    }
+
+    if ($(window).width() >= 320) {
+        initMagic();
+
+        $('.employees-table__col_lg').hover(function() {
+            $(this).closest('.employees-table__row').addClass('selected')
+        }, function() {
+            $(this).closest('.employees-table__row').removeClass('selected')
         });
-    })
-    .setPin('.js-decision__inner')
-    .setTween(timeLineStar)
-    .addTo(ctrl);
+    } else {
+        $('#arrows > g').each(function() {
+            var th = $(this),
+                st = th.find('.st1');
 
-    var timeLineAbout = new TimelineMax();
-    timeLineAbout
-        .to('.about-slide:nth-child(1)', 0.1, 
-            {
-                className: '+=zoom-in', 
-                delay: '0.05'
-            }
-        )
-        .to('.about-slide:nth-child(2)', 0.1, 
-            {
-                className: '+=active'
-            }
-        )
-        .to('.about-slide:nth-child(2)', 0.1, 
-            {
-                className: '+=zoom-in', 
-                onComplete: function() {
-                    $('.about-slide:nth-child(2)').removeClass('active')
-                }
-            }
-        )
-        .to('.about-slide:nth-child(3)', 0.1, 
-            {
-                className: '+=active'
-            }
-        )
-        .to('.about-slide:nth-child(3)', 0.1, 
-            {
-                className: '+=zoom-in', 
-                onComplete: function() {
-                    $('.about-slide:nth-child(2)').removeClass('active')
-                }
-            }
-        )
-        .fromTo('.about-slide:nth-child(4)', 0.1, 
-            {
-              className: '-=active', 
-            },
-            {
-              className: '+=active'
-            }
-        )
-  
-    var scene3 = new ScrollMagic.Scene({
-        triggerElement: '.about',
-        triggerHook: 'onLeave',
-        duration: '300%',
-    })
-    .setPin('.about')
-    .setTween(timeLineAbout)
-    .addTo(ctrl);
-
-    var sceneTable = new ScrollMagic.Scene({
-        triggerElement: '.employees-table'
-    })
-    .addTo(ctrl)
-    .on('enter', function() {
-        $('.employees-table').addClass('load').removeClass('start')
-    })
+                th.animate({'stroke-dashoffset': 0}, 1000, function() {
+                    st.animate({'stroke-dashoffset': 0}, 0)
+                })
+        });
+    }
 })
 
 var _scrollTop, _rotate = 0;
